1c28f29d1b44eb430b283672ec6d1adb5522a1b9
adyasingh27 <143116879+adyasingh27@users.noreply.github.com>
2025-06-24 22:15:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MyTuteeLanding from "./components/MyTuteeLanding";
+
+function App() {
+  return (
+    <div>
+      <MyTuteeLanding />
+    </div>
+  );
+}
+
+export default App;
